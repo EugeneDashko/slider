2532b850c7d3db2c2662a0b8a8ea94b349b323a9
EugeneDashko <eugenedashko@yahoo.com>
2023-10-02 22:14:03 +0300

    update files

--- slider.js
+++ slider.js
@@ -3,22 +3,17 @@ const images = [
 ];
 
 let activeImage = 0;
-
 const sliderPlace = document.querySelector('.slider-line');
-
 const widthOffset = document.querySelector('.slider').clientWidth;
-
-sliderPlace.style.width = 3 * widthOffset + 'px';
-sliderPlace.style.height = widthOffset + 'px';
-
-// document.querySelector('.slider').style.height = widthOffset + 'px';
-
-// let flag = true;
+sliderPlace.style.width = 3*widthOffset + 'px';
+sliderPlace.style.heigth = widthOffset + 'px';
+document.querySelector('.slider').style.heigth = widthOffset + 'px';
+let flag = true;
 
 
 const initSlider = () => {
     const img = document.createElement('img');
-    img.alt = 'picture';
+    img.alt = '';
     img.src = './images/' + images[activeImage];
     sliderPlace.append(img);
     nextImageGenerate();
@@ -26,6 +21,7 @@ const initSlider = () => {
 }
 
 const nextImageGenerate = () => {
+    // проверим какая следующая картинка
     let nextImage = activeImage + 1;
     if (nextImage >= images.length) {
         nextImage = 0;
@@ -37,84 +33,84 @@ const nextImageGenerate = () => {
 }
 
 const prevImageGenerate = (w = false) => {
+    // проверим какая следующая картинка
     let prevImage = activeImage - 1;
     if (prevImage < 0) {
         prevImage = images.length - 1;
     }
     const img = document.createElement('img');
     img.alt = '';
-    // if (w) img.style.width = 0;
+    if (w) img.style.width = 0;
     img.src = './images/' + images[prevImage];
     sliderPlace.prepend(img);
 }
 
 initSlider();
+// itgid.info
 
 const nextSlide = () => {
-    // if (!flag) return;
-    // flag = !flag;
+    if (!flag) return;
+    flag = !flag;
     
     activeImage++;
     if (activeImage >= images.length) {
         activeImage = 0;
     }
-    document.querySelector('.slider-line > img').remove();
     nextImageGenerate();
 
-    // animate({
-    //     duration: 1000,
-    //     draw: function(progress) {
-    //         document.querySelector('.slider-line > img').style.width = (widthOffset * (1- progress))+ 'px';
-    //     },
-    //     removeElement :     document.querySelector('.slider-line > img')    
-    // });
+    animate({
+        duration: 1000,
+        draw: function(progress) {
+            document.querySelector('.slider-line > img').style.width = (widthOffset * (1- progress))+ 'px';
+        },
+        removeElement :     document.querySelector('.slider-line > img')    
+    });
 }
 
 const prevSlide = () => {
-    // if (!flag) return;
-    // flag = !flag;
-
+    if (!flag) return;
+    flag = !flag;
+    
     activeImage--;
     if (activeImage < 0) {
         activeImage = images.length - 1;
     }
-    document.querySelector('.slider-line > img:last-child').remove();
-    prevImageGenerate();
+    prevImageGenerate(true);
 
-    // animate({
-    //     duration: 1000,
-    //     draw: function(progress) {
-    //         document.querySelector('.slider-line > img').style.width = (widthOffset * progress)+ 'px';
-    //     },
-    //     timing: function(step) {
-    //       return step;
-    //     },
-    //     removeElement :     document.querySelector('.slider-line > img:last-child')
-    //   });
-
+    animate({
+        duration: 1000,
+        draw: function(progress) {
+            document.querySelector('.slider-line > img').style.width = (widthOffset * progress)+ 'px';
+        },
+        timing: function(step) {
+          return step;
+        },
+        removeElement :     document.querySelector('.slider-line > img:last-child')      
+      });
+ 
 }
 
 
 document.querySelector('.next-button').addEventListener('click', nextSlide);
 document.querySelector('.prev-button').addEventListener('click', prevSlide);
 
-// function animate({duration, draw, removeElement}) {
+function animate({duration, draw, removeElement}) {
 
-//     let start = performance.now();
+    let start = performance.now();
+  
+    requestAnimationFrame(function animate(time) {
+      let step = (time - start) / duration;
 
-//     requestAnimationFrame(function animate(time) {
-//       let step = (time - start) / duration;
-
-//       if (step > 1) step = 1;
-
-//       draw(step);
-
-//       if (step < 1) {
-//         requestAnimationFrame(animate);
-//       }
-//       else {
-//         removeElement.remove();
-//         flag = true;
-//       }
-//     });
-//   }
+      if (step > 1) step = 1;
+  
+      draw(step);
+  
+      if (step < 1) {
+        requestAnimationFrame(animate);
+      }
+      else {
+        removeElement.remove();
+        flag = true;
+      }
+    });
+  }
